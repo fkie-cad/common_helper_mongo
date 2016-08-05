e127a30d711a788ec656d0939081f6773c6278ae
Peter Weidenbach <weidenba@cs.uni-bonn.de>
2016-08-05 12:44:10 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import subprocess
 from setuptools import setup, find_packages
 
 setup(
-    name="common_helper_files",
+    name="common_helper_mongo",
     version=subprocess.check_output(['git', 'describe', '--always'], cwd=os.path.dirname(os.path.abspath(__file__))).strip().decode('utf-8'),
     packages=find_packages(),
 )
