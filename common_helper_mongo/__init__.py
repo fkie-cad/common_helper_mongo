1be5dcfa158daaa01559e8766ac0fb13942427b7
Peter Weidenbach <weidenba@cs.uni-bonn.de>
2016-12-13 14:55:41 +0100

    function exported

--- common_helper_mongo/__init__.py
+++ common_helper_mongo/__init__.py
@@ -1,10 +1,11 @@
 from .gridfs import overwrite_file
-from .aggregate import get_objects_and_count_of_occurrence, get_field_average, get_field_sum, get_field_execute_operation
+from .aggregate import get_objects_and_count_of_occurrence, get_field_average, get_field_sum, get_field_execute_operation, get_list_of_all_values
 
 __all__ = [
            'overwrite_file',
            'get_objects_and_count_of_occurrence',
            'get_field_average',
            'get_field_sum',
-           'get_field_execute_operation'
+           'get_field_execute_operation',
+           'get_list_of_all_values'
         ]
