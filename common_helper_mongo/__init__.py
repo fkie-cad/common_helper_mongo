dd0e40ebe4cfa325a691a365a9d41525c9c1a87c
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-02-12 15:33:25 +0100

    fixed tests, added config files

--- common_helper_mongo/__init__.py
+++ common_helper_mongo/__init__.py
@@ -1,6 +1,7 @@
-from .aggregate import (get_field_average, get_field_execute_operation,
-                        get_field_sum, get_list_of_all_values,
-                        get_objects_and_count_of_occurrence)
+from .aggregate import (
+    get_field_average, get_field_execute_operation, get_field_sum, get_list_of_all_values,
+    get_objects_and_count_of_occurrence
+)
 from .gridfs import overwrite_file
 
 __all__ = [
