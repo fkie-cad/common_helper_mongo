327d0a042e4e20b5ccb5d1961be0820194f21b59
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-02-12 15:23:14 +0100

    removed deprecated function

--- common_helper_mongo/__init__.py
+++ common_helper_mongo/__init__.py
@@ -1,5 +1,7 @@
+from .aggregate import (get_field_average, get_field_execute_operation,
+                        get_field_sum, get_list_of_all_values,
+                        get_objects_and_count_of_occurrence)
 from .gridfs import overwrite_file
-from .aggregate import get_objects_and_count_of_occurrence, get_field_average, get_field_sum, get_field_execute_operation, get_list_of_all_values, get_list_of_all_values_and_collect_information_of_additional_field
 
 __all__ = [
     'overwrite_file',
@@ -8,5 +10,4 @@ __all__ = [
     'get_field_sum',
     'get_field_execute_operation',
     'get_list_of_all_values',
-    'get_list_of_all_values_and_collect_information_of_additional_field'
 ]
